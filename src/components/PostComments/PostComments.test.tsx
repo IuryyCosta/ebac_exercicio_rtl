3c881ba9ddeb44b76527d822a3af553c10449b0d
iuryyCosta <iury.costa2209@gmail.com>
2025-05-22 21:27:18 -0300

    Ajuste para realizar test

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,18 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Post from './index';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+test('insere dois comentários', () => {
+  render(<Post />);
+
+  const input = screen.getByTestId('comment-input') as HTMLTextAreaElement;
+  const button = screen.getByTestId('submit-button');
+
+  fireEvent.change(input, { target: { value: 'Primeiro comentário' } });
+  fireEvent.click(button);
+
+  fireEvent.change(input, { target: { value: 'Segundo comentário' } });
+  fireEvent.click(button);
+
+  expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+  expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
 });
